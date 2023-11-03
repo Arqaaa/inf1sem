e4d36c283a5dfe6aff361ea728964a38e1aed154
Arqaaa <tanx00@mail.ru>
2023-11-03 14:38:26 +0300

    1111

---  7 sem/1.py
+++  7 sem/1.py
@@ -1,5 +1,5 @@
 class Vector() :
-    def __init__(self , x = 1 , y = 1 , z = 1):
+    def __init__(self , x = 0 , y = 0 , z = 0):
         self.x = x
         self.y = y
         self.z = z
@@ -25,6 +25,11 @@ class Vector() :
         self.x = A[0]
         self.y = A[1]
         self.z = A[2]
+    def __sec__(self , other):
+        return 0.5 * (Vector(self.y * other.z - self.z * other.y , self.z * other.x - self.x * other.z , self.x * other.y - self.y * other.x).__abs__())
+
+
+
 
 if __name__  == "__main__" :
 
@@ -39,7 +44,24 @@ if __name__  == "__main__" :
     M = Vector(0 , 0 , 0)
     for i in range(len(v)) :
         M += v[i]
-
     C = M.__matmul__(1/len(v))
-
     print(C)
+    print(v[0].__sec__(v[1]))
+    L = []
+    l = []
+    S = []
+
+    for i in range(len(v)):
+        for j in range(i + 1, len(v)):
+            for k in range(j + 1 , len(v)):
+                a = Vector()
+                b = Vector()
+                a += v[i] + v[j].__matmul__(-1)
+                b += v[i] + v[k].__matmul__(-1)
+                s = a.__sec__(b)
+                S.append(s)
+
+    print(max(S))
+
+
+
